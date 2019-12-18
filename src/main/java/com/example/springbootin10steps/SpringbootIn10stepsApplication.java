22e684cdfa8eedf71eb37613e07bf77e01d68e3d
aravannai <38413795+aravannai@users.noreply.github.com>
2019-12-18 07:39:37 +0530

    Initial commit

---
+++ src/main/java/com/example/springbootin10steps/SpringbootIn10stepsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootin10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIn10stepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIn10stepsApplication.class, args);
+	}
+
+}
